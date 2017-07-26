7bde9e34962497bf19d9394eddc595297d9d8ae1
Don Park <donp@donp.org>
2017-07-26 12:25:09 -0700

    quotes

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo in-container setup starting
-while [ -z `ifconfig eth0 |grep inet` ] ; do echo waiting for eth0; sleep 1;done
+while [ -z "`ifconfig eth0 |grep inet`" ] ; do echo waiting for eth0; sleep 1;done
 apk update
 apk add expat
 apk add python3
 pip3 install protobuf websocket-client
-chmod 755 /etc/init.d/cointhink-script 
+chmod 755 /etc/init.d/cointhink-script
 rc-update add cointhink-script default
-rc-status 
+rc-status
 rc-status -c
 rm /setup.sh
