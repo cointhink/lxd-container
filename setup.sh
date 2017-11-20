026798e1dc42bc3fcf57848499a8ad2e2d18e429
Don Park <donp@donp.org>
2017-11-20 12:52:09 +0700

    numpy

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ while [ -z "`ifconfig eth0 |grep inet\ addr`" ] ; do echo waiting for eth0; slee
 apk update
 apk add expat
 apk add python3
+apk add py3-numpy
 pip3 install protobuf websocket-client
 cp /cointhink/websocket-0.44.0-noerr-_app.py /usr/lib/python3.6/site-packages/websocket/_app.py || echo '!!! websocket patch FAILED'
 chmod 755 /etc/init.d/cointhink-script
